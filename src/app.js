2e18135fbf1bbc3a7a98561690b889be951708c8
Zachary Moss <zachary.moss@ymail.com>
2017-03-16 17:17:19 -0500

    Added log-in-out switch

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm.js';
 
 class App extends Component {
+    state = { loggedIn: null };
+
     componentWillMount() {
         firebase.initializeApp({
     apiKey: 'AIzaSyDs4mNFzoVLa1xT1c0IY8go91EuX7TxG4w',
@@ -13,12 +15,36 @@ class App extends Component {
     storageBucket: 'auth-b9b43.appspot.com',
     messagingSenderId: '1041162122242'
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
     }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                <Button onPress={() => firebase.auth().signOut()}>
+                    Log Out
+                    </Button>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner size="large" />;
+        }
+    }
+
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
